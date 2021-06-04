2f731d4371a128e540e0848f7d1d8cbde5c744da
Elara Damasceno <elaradamasceno.edl@gmail.com>
2021-06-04 13:12:59 -0300

    Google Books API request as typed in input

--- src/components/InputSearch.js
+++ src/components/InputSearch.js
@@ -1,11 +1,39 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { Input } from 'antd';
 
+import api from '../services/api';
+
 export function InputSearch(){
+  const [startIndex, setStartIndex] = useState(0);
+
+  function handleChange(e){
+    let value = e.target.value;
+    setStartIndex(0);
+
+    if(value)
+      getBooks(value);
+  }
+
+  function getBooks(value){
+    api.get(`volumes?q=${value}&startIndex=${startIndex}`)
+    .then(res => {
+      console.log('result ', res)
+    })
+    .catch(err => {
+      // chamar página 404
+      console.log('error ', err)
+    })
+  }
+  
   return(
     <div className="search">
       <div className="content-search">
-        <Input size="large" placeholder="Pesquise aqui..." allowClear />
+        <Input 
+          size="large" 
+          placeholder="Pesquise aqui..." 
+          allowClear 
+          onChange={handleChange}
+        />
       </div>
     </div>
   )
