e164d68ed84d43b02918b1ebf6169c0e1dca29b3
Elara Damasceno <elaradamasceno.edl@gmail.com>
2021-06-04 12:23:22 -0300

    config/install eslint, axios and ant design

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
+import React from 'react';
+import { Home } from './components/Home';
+import './styles/css/global.css';
+
 function App() {
   return (
     <div className="App">
-      
+      <Home />
     </div>
   );
 }
---
+++ src/components/Home.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+export function Home(){
+  return(
+    <div className="home">
+      <header>
+
+      </header>
+
+      <main>
+
+      </main>
+      
+      <footer>
+
+      </footer>
+    </div>
+  )
+}
