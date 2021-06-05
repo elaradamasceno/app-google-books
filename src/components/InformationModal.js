13dfd3b51db70e011cbb56927cd433ddfe145a61
Elara Damasceno <elaradamasceno.edl@gmail.com>
2021-06-04 23:00:54 -0300

    video information modal - in progress

---
+++ src/components/InformationModal.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Modal } from 'antd';
+
+export function InformationModal({isModalVisible}){
+  function handleMore(){
+    
+  }
+
+  function handleClose(){
+
+  }
+
+  return(
+    <Modal title="Basic Modal" visible={isModalVisible} onOk={handleMore} onCancel={handleClose} okText="+" cancelText="Fechar">
+      <p>Some contents...</p>
+      <p>Some contents...</p>
+      <p>Some contents...</p>
+    </Modal>
+  )
+}
